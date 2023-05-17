4527abd475a7ef0a2b8652ae443ca265eac9aa77
Cedric <50719841+mutonic@users.noreply.github.com>
2023-05-17 07:41:46 +0200

    resolve linters errors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hamburgerIcon = hamburger.querySelector('span');
 const mobileMenu = document.querySelector('.mobile-nav');
 const closeHam = document.querySelector('.close-menu');
 const closeIcon = closeHam.querySelector('span');
-// const links = mobileMenu.querySelectorAll('a');
+const links = mobileMenu.querySelectorAll('a');
 
 function toggleMenu() {
   hamburgerIcon.innerText = hamburgerIcon.innerText === 'menu' ? 'close' : 'menu';
@@ -23,5 +23,5 @@ links.forEach((link) => {
   link.addEventListener('click', () => {
     hamburgerIcon.innerText = 'menu';
     mobileMenu.classList.remove('active');
-  });
+  });
 });
