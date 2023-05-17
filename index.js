0b2c976d2106ed4485fcc754b177db911ccd7cc6
Cedric <50719841+mutonic@users.noreply.github.com>
2023-05-17 02:10:57 +0200

    Add a popup on the menu

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const hamburger = document.querySelector('.hamburger-menu');
 const hamburgerIcon = hamburger.querySelector('span');
-const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenu = document.querySelector('.mobile-nav');
+const closeHam = document.querySelector('.close-menu');
+const closeIcon = closeHam.querySelector('span');
+
+//  const links = mobileMenu.querySelectorAll('a');
 
 hamburger.addEventListener('click', () => {
   hamburgerIcon.innerText = hamburgerIcon.innerText === 'menu' ? 'close' : 'menu';
-  mobileMenu.classList.toggle('active')
+  mobileMenu.classList.toggle('active');
+});
+
+closeHam.addEventListener('click', () => {
+  hamburgerIcon.innerText = 'menu';
+  closeIcon.innerText = 'close';
+  mobileMenu.classList.remove('active');
 });
