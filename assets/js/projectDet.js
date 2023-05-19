89f09a6c18d28a3e1075e152d2fc58c24a436297
Cedric <50719841+mutonic@users.noreply.github.com>
2023-05-19 20:46:21 +0200

    resolve linters errors

--- assets/js/projectDet.js
+++ assets/js/projectDet.js
@@ -2,64 +2,64 @@ const projectHistory = [
   {
     projectTitle: 'Multi-Post Stories <br>Gain+Glory',
     projectTechno: ['Ruby', 'CSS', 'Javascript', 'HTML'],
-    name: "Keeping track of hundreds of components",
+    name: 'Keeping track of hundreds of components',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
-    image: "./assets/project_detailsImg.png",
-    liveLink: "https://mutonic.github.io/Portfolio_Microverse/",
-    sourceLink: "https://github.com/mutonic/Portfolio_Microverse",
+    image: './assets/project_detailsImg.png',
+    liveLink: 'https://mutonic.github.io/Portfolio_Microverse/',
+    sourceLink: 'https://github.com/mutonic/Portfolio_Microverse',
   },
   {
-    projectTitle: 'Multi-Post Stories<br>Gain+Glory',
+    projectTitle: 'Multi-Post Stories <br>Gain+Glory',
     projectTechno: ['Ruby', 'CSS', 'Javascript', 'HTML'],
-    name: "Keeping track of hundreds of components",
+    name: 'Keeping track of hundreds of components',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
-    image: "./assets/project_detailsImg.png",
-    liveLink: "https://mutonic.github.io/Portfolio_Microverse/",
-    sourceLink: "https://github.com/mutonic/Portfolio_Microverse",
+    image: './assets/project_detailsImg.png',
+    liveLink: 'https://mutonic.github.io/Portfolio_Microverse/',
+    sourceLink: 'https://github.com/mutonic/Portfolio_Microverse',
   },
   {
-    projectTitle: 'Multi-Post Stories<br>Gain+Glory',
+    projectTitle: 'Multi-Post Stories <br>Gain+Glory',
     projectTechno: ['Ruby', 'CSS', 'Javascript', 'HTML'],
-    name: "Keeping track of hundreds of components",
+    name: 'Keeping track of hundreds of components',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
-    image: "./assets/project_detailsImg.png",
-    liveLink: "https://mutonic.github.io/Portfolio_Microverse/",
-    sourceLink: "https://github.com/mutonic/Portfolio_Microverse",
+    image: './assets/project_detailsImg.png',
+    liveLink: 'https://mutonic.github.io/Portfolio_Microverse/',
+    sourceLink: 'https://github.com/mutonic/Portfolio_Microverse',
   },
   {
-    projectTitle: 'Multi-Post Stories<br>Gain+Glory',
+    projectTitle: 'Multi-Post Stories <br>Gain+Glory',
     projectTechno: ['Ruby', 'CSS', 'Javascript', 'HTML'],
-    name: "Keeping track of hundreds of components",
+    name: 'Keeping track of hundreds of components',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
-    image: "./assets/project_detailsImg.png",
-    liveLink: "https://mutonic.github.io/Portfolio_Microverse/",
-    sourceLink: "https://github.com/mutonic/Portfolio_Microverse",
+    image: './assets/project_detailsImg.png',
+    liveLink: 'https://mutonic.github.io/Portfolio_Microverse/',
+    sourceLink: 'https://github.com/mutonic/Portfolio_Microverse',
   },
   {
-    projectTitle: 'Multi-Post Stories<br>Gain+Glory',
+    projectTitle: 'Multi-Post Stories <br>Gain+Glory',
     projectTechno: ['Ruby', 'CSS', 'Javascript', 'HTML'],
-    name: "Keeping track of hundreds of components",
+    name: 'Keeping track of hundreds of components',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
-    image: "./assets/project_detailsImg.png",
-    liveLink: "https://mutonic.github.io/Portfolio_Microverse/",
-    sourceLink: "https://github.com/mutonic/Portfolio_Microverse",
+    image: './assets/project_detailsImg.png',
+    liveLink: 'https://mutonic.github.io/Portfolio_Microverse/',
+    sourceLink: 'https://github.com/mutonic/Portfolio_Microverse',
   },
   {
-    projectTitle: 'Multi-Post Stories<br>Gain+Glory',
+    projectTitle: 'Multi-Post Stories <br>Gain+Glory',
     projectTechno: ['Ruby', 'CSS', 'Javascript', 'HTML'],
-    name: "Keeping track of hundreds of components",
+    name: 'Keeping track of hundreds of components',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
-    image: "./assets/project_detailsImg.png",
-    liveLink: "https://mutonic.github.io/Portfolio_Microverse/",
-    sourceLink: "https://github.com/mutonic/Portfolio_Microverse",
+    image: './assets/project_detailsImg.png',
+    liveLink: 'https://mutonic.github.io/Portfolio_Microverse/',
+    sourceLink: 'https://github.com/mutonic/Portfolio_Microverse',
   },
 ];
 
 const workContainer = document.getElementById('project-container');
 let projectHTML = '';
-for (let i = 0; i < projectHistory.length; i++) {
+for (let i = 0; i < projectHistory.length; i += 1) {
   const project = projectHistory[i];
-  const projectTags = project.projectTechno.map(tag => `<li>${tag}</li>`).join('');
+  const projectTags = project.projectTechno.map((tag) => `<li>${tag}</li>`).join('');
   projectHTML += `
     <div class="project-grid">
       <div class="project-image"></div>
@@ -79,77 +79,10 @@ for (let i = 0; i < projectHistory.length; i++) {
 workContainer.innerHTML = projectHTML;
 
 /* Project Popup */
-const workSec = document.querySelector(".project");
-const openProjectButtons = document.querySelectorAll(".button");
-
-openProjectButtons.forEach((button, index) => {
-  button.addEventListener("click", () => {
-    const project = projectHistory[index];
-
-    const projectSect = document.createElement("section");
-    projectSect.classList.add("project-section");
-
-    const projectContainer = document.createElement("div");
-    projectContainer.classList.add("project-container");
-
-    const closeButton = document.createElement("button");
-    closeButton.classList.add("close-project");
-    closeButton.innerHTML = `
-      <span class="material-symbols-outlined"> close </span>
-    `;
-    closeButton.addEventListener("click", () => {
-      projectSect.remove();
-    });
-    projectContainer.appendChild(closeButton);
-
-    const projImage = document.createElement("div");
-    projImage.classList.add("proj-image");
-    projImage.innerHTML = `
-      <img src="${project.image}" alt="project image" />
-    `;
-    projectContainer.appendChild(projImage);
-
-    const projTitle = document.createElement("div");
-    projTitle.classList.add("proj-title");
-    projTitle.innerHTML = `
-      <h2>${project.projectTitle}</h2>
-    `;
-    projectContainer.appendChild(projTitle);
-
-    const projectTechno = document.createElement("div");
-    projectTechno.classList.add("project-techno");
-    const technologiesList = project.projectTechno
-      .map(tag => `<li>${tag}</li>`)
-      .join("");
-    projectTechno.innerHTML = `
-      <ul>${technologiesList}</ul>
-    `;
-    projectContainer.appendChild(projectTechno);
-
-    const projectDetail = document.createElement("div");
-    projectDetail.classList.add("project-detail");
-    projectDetail.innerHTML = `
-      <p>${project.description}</p>
-    `;
-    projectContainer.appendChild(projectDetail);
-
-    const projectButtons = document.createElement("div");
-    projectButtons.classList.add("project-buttons");
-    const liveButton = createButton("See Live", project.liveLink, "./assets/see_live_icon.svg");
-    const sourceButton = createButton("See Source", project.sourceLink, "./assets/github_icon.svg");
-    projectButtons.appendChild(liveButton);
-    projectButtons.appendChild(sourceButton);
-    projectContainer.appendChild(projectButtons);
-
-    projectSect.appendChild(projectContainer);
-    workSec.appendChild(projectSect);
-  });
-});
-
 function createButton(label, link, iconSrc) {
-  const button = document.createElement("button");
-  button.classList.add("project-source");
-  const anchor = document.createElement("a");
+  const button = document.createElement('button');
+  button.classList.add('project-source');
+  const anchor = document.createElement('a');
   anchor.href = link;
   anchor.innerHTML = `
     ${label}
@@ -158,3 +91,70 @@ function createButton(label, link, iconSrc) {
   button.appendChild(anchor);
   return button;
 }
+
+const workSec = document.querySelector('.project');
+const openProjectButtons = document.querySelectorAll('.button');
+
+openProjectButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    const project = projectHistory[index];
+
+    const projectSect = document.createElement('section');
+    projectSect.classList.add('project-section');
+
+    const projectContainer = document.createElement('div');
+    projectContainer.classList.add('project-container');
+
+    const closeButton = document.createElement('button');
+    closeButton.classList.add('close-project');
+    closeButton.innerHTML = `
+      <span class="material-symbols-outlined"> close </span>
+    `;
+    closeButton.addEventListener('click', () => {
+      projectSect.remove();
+    });
+    projectContainer.appendChild(closeButton);
+
+    const projImage = document.createElement('div');
+    projImage.classList.add('proj-image');
+    projImage.innerHTML = `
+      <img src="${project.image}" alt="project image" />
+    `;
+    projectContainer.appendChild(projImage);
+
+    const projTitle = document.createElement('div');
+    projTitle.classList.add('proj-title');
+    projTitle.innerHTML = `
+      <h2>${project.projectTitle}</h2>
+    `;
+    projectContainer.appendChild(projTitle);
+
+    const projectTechno = document.createElement('div');
+    projectTechno.classList.add('project-techno');
+    const technologiesList = project.projectTechno
+      .map((tag) => `<li>${tag}</li>`)
+      .join('');
+    projectTechno.innerHTML = `
+      <ul>${technologiesList}</ul>
+    `;
+    projectContainer.appendChild(projectTechno);
+
+    const projectDetail = document.createElement('div');
+    projectDetail.classList.add('project-detail');
+    projectDetail.innerHTML = `
+      <p>${project.description}</p>
+    `;
+    projectContainer.appendChild(projectDetail);
+
+    const projectButtons = document.createElement('div');
+    projectButtons.classList.add('project-buttons');
+    const liveButton = createButton('See Live', project.liveLink, './assets/see_live_icon.svg');
+    const sourceButton = createButton('See Source', project.sourceLink, './assets/github_icon.svg');
+    projectButtons.appendChild(liveButton);
+    projectButtons.appendChild(sourceButton);
+    projectContainer.appendChild(projectButtons);
+
+    projectSect.appendChild(projectContainer);
+    workSec.appendChild(projectSect);
+  });
+});
