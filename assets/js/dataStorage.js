0ef08507df2a7c97fc22ed1608feb300c5e82663
Cedric <50719841+mutonic@users.noreply.github.com>
2023-05-20 13:39:48 +0200

    Add data storage functionality

---
+++ assets/js/dataStorage.js
@@ -0,0 +1,26 @@
+// Local storage
+const nameInput = document.getElementById('fullName');
+const emailInput = document.getElementById('email-input');
+const messageInput = document.getElementById('message');
+const allInputs = document.querySelectorAll('input');
+
+const storeData = () => {
+  const inputData = {
+    inputName: nameInput.value,
+    inputEmail: emailInput.value,
+    inputText: messageInput.value,
+  };
+  localStorage.setItem('UserData', JSON.stringify(inputData));
+};
+
+allInputs.forEach((input) => {
+  input.addEventListener('input', storeData);
+});
+
+const prefillData = JSON.parse(localStorage.getItem('UserData'));
+
+if (prefillData) {
+  nameInput.value = prefillData.inputName;
+  emailInput.value = prefillData.inputEmail;
+  messageInput.value = prefillData.inputText;
+}
