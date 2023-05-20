48d6e4ad6aedf6d55a9060d1d4cbafb1fc508a6e
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-05-20 13:29:38 +0600

    Add functionality to create source and live buttons

--- assets/js/projectDet.js
+++ assets/js/projectDet.js
@@ -79,7 +79,18 @@ for (let i = 0; i < projectHistory.length; i += 1) {
 workContainer.innerHTML = projectHTML;
 
 /* Project Popup */
-
+function createButton(label, link, iconSrc) {
+  const button = document.createElement("button");
+  button.classList.add("project-source");
+  const anchor = document.createElement("a");
+  anchor.href = link;
+  anchor.innerHTML = `
+    ${label}
+    <img src="${iconSrc}" alt="github logo" />
+  `;
+  button.appendChild(anchor);
+  return button;
+};
 
 const workSec = document.querySelector('.project');
 const openProjectButtons = document.querySelectorAll('.button');
